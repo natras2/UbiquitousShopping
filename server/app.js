4071a296008b729e80e37fd617408a8643c33a72
natras2 <56722995+natras2@users.noreply.github.com>
2023-09-23 13:49:55 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -6,16 +6,16 @@
 
 var express = require('express');
 var app = express();
-var port = 3000;
+const port = process.env.PORT || 8080;
 
 // create an error with .status. we
 // can then use the property in our
 // custom error handler (Connect respects this prop as well)
 
 function error(status, msg) {
-  var err = new Error(msg);
-  err.status = status;
-  return err;
+    var err = new Error(msg);
+    err.status = status;
+    return err;
 }
 
 // if we wanted to supply more than JSON, we could
@@ -28,17 +28,17 @@ function error(status, msg) {
 // will cause this middleware to be invoked
 
 app.use('/api', function(req, res, next){
-  var key = req.query['api-key'];
+    var key = req.query['api-key'];
 
-  // key isn't present
-  if (!key) return next(error(400, 'api key required'));
+    // key isn't present
+    if (!key) return next(error(400, 'api key required'));
 
-  // key is invalid
-  if (apiKeys.indexOf(key) === -1) return next(error(401, 'invalid api key'))
+    // key is invalid
+    if (apiKeys.indexOf(key) === -1) return next(error(401, 'invalid api key'))
 
-  // all good, store req.key for route access
-  req.key = key;
-  next();
+    // all good, store req.key for route access
+    req.key = key;
+    next();
 });
 
 // map of valid api keys, typically mapped to
@@ -51,21 +51,21 @@ var apiKeys = ['foo', 'bar', 'baz'];
 // these two objects will serve as our faux database
 
 var repos = [
-  { name: 'express', url: 'https://github.com/expressjs/express' },
-  { name: 'stylus', url: 'https://github.com/learnboost/stylus' },
-  { name: 'cluster', url: 'https://github.com/learnboost/cluster' }
+    { name: 'express', url: 'https://github.com/expressjs/express' },
+    { name: 'stylus', url: 'https://github.com/learnboost/stylus' },
+    { name: 'cluster', url: 'https://github.com/learnboost/cluster' }
 ];
 
 var users = [
-  { name: 'tobi' }
-  , { name: 'loki' }
-  , { name: 'jane' }
+    { name: 'tobi' }
+    , { name: 'loki' }
+    , { name: 'jane' }
 ];
 
 var userRepos = {
-  tobi: [repos[0], repos[1]]
-  , loki: [repos[1]]
-  , jane: [repos[2]]
+    tobi: [repos[0], repos[1]]
+    , loki: [repos[1]]
+    , jane: [repos[2]]
 };
 
 // we now can assume the api key is valid,
@@ -79,21 +79,21 @@ app.get('/', function (req, res) {
 
 // example: http://localhost:3000/api/users/?api-key=foo
 app.get('/api/users', function (req, res) {
-  res.send(users);
+    res.send(users);
 });
 
 // example: http://localhost:3000/api/repos/?api-key=foo
 app.get('/api/repos', function (req, res) {
-  res.send(repos);
+    res.send(repos);
 });
 
 // example: http://localhost:3000/api/user/tobi/repos/?api-key=foo
 app.get('/api/user/:name/repos', function(req, res, next){
-  var name = req.params.name;
-  var user = userRepos[name];
+    var name = req.params.name;
+    var user = userRepos[name];
 
-  if (user) res.send(user);
-  else next();
+    if (user) res.send(user);
+    else next();
 });
 
 // middleware with an arity of 4 are considered
@@ -102,22 +102,20 @@ app.get('/api/user/:name/repos', function(req, res, next){
 // in order, but ONLY those with an arity of 4, ignoring
 // regular middleware.
 app.use(function(err, req, res, next){
-  // whatever you want here, feel free to populate
-  // properties on `err` to treat it differently in here.
-  res.status(err.status || 500);
-  res.send({ error: err.message });
+    // whatever you want here, feel free to populate
+    // properties on `err` to treat it differently in here.
+    res.status(err.status || 500);
+    res.send({ error: err.message });
 });
 
 // our custom JSON 404 middleware. Since it's placed last
 // it will be the last middleware called, if all others
 // invoke next() and do not respond.
 app.use(function(req, res){
-  res.status(404);
-  res.send({ error: "Sorry, can't find that" })
+    res.status(404);
+    res.send({ error: "Sorry, can't find that" })
 });
 
-/* istanbul ignore next */
-if (!module.main) {
-  app.listen(port);
-  console.log('Express started on port 3000');
-}
+app.listen(port, () => {
+    console.log('Hello world listening on port', port);
+});
