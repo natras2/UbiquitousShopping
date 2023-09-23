43dc5ba6a0abf164007e5c734b789e4fc55dbff1
natras2 <56722995+natras2@users.noreply.github.com>
2023-09-23 17:24:39 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,116 +1,71 @@
-var express = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 require('dotenv').config();
 
 var app = express();
 const port = 8080;
-const secretKey = process.env.SECRET_KEY || "1234567890"
+const secretKey = process.env.SECRET_KEY;
 
-// create an error with .status. we
-// can then use the property in our
-// custom error handler (Connect respects this prop as well)
+app.use(bodyParser.json());
 
-function error(status, msg) {
-    var err = new Error(msg);
-    err.status = status;
-    return err;
+const clients = [];
+
+// Middleware to check if a request is authenticated
+function authenticateToken(req, res, next) {
+    const token = req.header('Authorization').split(' ')[1];
+    if (!token) return res.sendStatus(401); // Unauthorized
+
+    jwt.verify(token, secretKey, (err, user) => {
+        if (err) {
+            console.error(err);
+            return res.sendStatus(403); // Forbidden
+        } 
+        req.user = user;
+        next();
+    });
 }
 
-// if we wanted to supply more than JSON, we could
-// use something similar to the content-negotiation
-// example.
-
-// here we validate the API key,
-// by mounting this middleware to /api
-// meaning only paths prefixed with "/api"
-// will cause this middleware to be invoked
-
-app.use('/api', function(req, res, next){
-    var key = req.query['api-key'];
-
-    // key isn't present
-    if (!key) return next(error(400, 'api key required'));
-
-    // key is invalid
-    if (apiKeys.indexOf(key) === -1) return next(error(401, 'invalid api key'))
-
-    // all good, store req.key for route access
-    req.key = key;
-    next();
+// Register a new user
+app.post('/register', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = { username: req.body.username, password: hashedPassword };
+        clients.push(user);
+        res.status(201).send('User registered successfully');
+    } catch(error) {
+        console.error(error)
+        res.status(500).send('Error registering user');
+    }
 });
 
-// map of valid api keys, typically mapped to
-// account info with some sort of database like redis.
-// api keys do _not_ serve as authentication, merely to
-// track API usage or help prevent malicious behavior etc.
+// Log in and generate a JWT
+app.post('/login', async (req, res) => {
+    const user = clients.find(u => u.username === req.body.username);
+    if (user == null) return res.status(400).send('User not found');
 
-var apiKeys = ['foo', 'bar', 'baz'];
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            const token = jwt.sign({ username: user.username }, secretKey, { expiresIn: '1h' });
+            res.json({ token });
+        } else {
+            res.status(401).send('Authentication failed');
+        }
+    } catch {
+        res.status(500).send('Error authenticating user');
+    }
+});
 
-// these two objects will serve as our faux database
-
-var repos = [
-    { name: 'express', url: 'https://github.com/expressjs/express' },
-    { name: 'stylus', url: 'https://github.com/learnboost/stylus' },
-    { name: 'cluster', url: 'https://github.com/learnboost/cluster' }
-];
-
-var users = [
-    { name: 'tobi' }
-    , { name: 'loki' }
-    , { name: 'jane' }
-];
-
-var userRepos = {
-    tobi: [repos[0], repos[1]]
-    , loki: [repos[1]]
-    , jane: [repos[2]]
-};
-
-// we now can assume the api key is valid,
-// and simply expose the data
+// Protected route - only accessible with a valid token
+app.get('/protected', authenticateToken, (req, res) => {
+    res.json(`Welcome, ${req.user.username}!`);
+});
 
 // example: http://localhost:3000/
 app.get('/', function (req, res) {
-    var obj = {message: "Hello world!", sk: secretKey};
+    let obj = { message: "Hello world!" };
     res.send(obj);
-});
-
-// example: http://localhost:3000/api/users/?api-key=foo
-app.get('/api/users', function (req, res) {
-    res.send(users);
-});
-
-// example: http://localhost:3000/api/repos/?api-key=foo
-app.get('/api/repos', function (req, res) {
-    res.send(repos);
-});
-
-// example: http://localhost:3000/api/user/tobi/repos/?api-key=foo
-app.get('/api/user/:name/repos', function(req, res, next){
-    var name = req.params.name;
-    var user = userRepos[name];
-
-    if (user) res.send(user);
-    else next();
-});
-
-// middleware with an arity of 4 are considered
-// error handling middleware. When you next(err)
-// it will be passed through the defined middleware
-// in order, but ONLY those with an arity of 4, ignoring
-// regular middleware.
-app.use(function(err, req, res, next){
-    // whatever you want here, feel free to populate
-    // properties on `err` to treat it differently in here.
-    res.status(err.status || 500);
-    res.send({ error: err.message });
-});
-
-// our custom JSON 404 middleware. Since it's placed last
-// it will be the last middleware called, if all others
-// invoke next() and do not respond.
-app.use(function(req, res){
-    res.status(404);
-    res.send({ error: "Sorry, can't find that" })
 });
 
 app.listen(port, () => {
