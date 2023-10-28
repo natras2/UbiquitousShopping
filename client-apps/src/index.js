46275350e1cb70fdc3df71d2f50b2eb820dbdb55
natras2 <56722995+natras2@users.noreply.github.com>
2023-10-28 18:10:43 +0200

    Update client apps domain reference

--- client-apps/src/index.js
+++ client-apps/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/css/style.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import App from './App';
 import Error from './Error';
