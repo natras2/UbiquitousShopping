5a22f6dc133e0f307a8f6e4cc155a6df8852432b
zanattopaolo1 <paolo.zanatto@studio.unibo.it>
2023-10-30 18:06:02 +0100

    Update of Cart Creation
    Brief comments to reason about the next time

--- server/shopping/controller/cart_creation.js
+++ server/shopping/controller/cart_creation.js
@@ -7,8 +7,11 @@ async function CartCreation(data) {
 
     const newCart = await Cart.create({
         generation_date: timestamp,
-        is_closed: 0,
-                
+        is_closed: 0
+        //Here I'm thinking to change the FK since when I create a new Cart it would be necessary to set the 
+        //Customer which it belongs and the Store where it is opened: 
+        //FK of Customer
+        //FK of Store        
     });
     
     return newCart;
