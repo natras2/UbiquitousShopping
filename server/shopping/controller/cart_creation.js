515806162366da64f5a4ef560584271aef72740b
natras2 <56722995+natras2@users.noreply.github.com>
2023-10-30 19:32:54 +0100

    Update index endpoint

--- server/shopping/controller/cart_creation.js
+++ server/shopping/controller/cart_creation.js
@@ -1,12 +1,11 @@
-//const jwt = require('jsonwebtoken');
-//const bcrypt = require('bcrypt');
 const Cart = require('../../model/cart');
 
 async function CartCreation(data) {
-    const timestamp = new Date().getTime();
+    // manipulate dates in the format YYYY-MM-DD, as it is the only one handled by MySQL DB
+    const date = new Date().toISOString().split("T", 1)[0]; 
 
     const newCart = await Cart.create({
-        generation_date: timestamp,
+        generation_date: date,
         is_closed: 0
         //Here I'm thinking to change the FK since when I create a new Cart it would be necessary to set the 
         //Customer which it belongs and the Store where it is opened: 
@@ -17,4 +16,4 @@ async function CartCreation(data) {
     return newCart;
 }
 
-module.exports = CartAccess;
+module.exports = CartCreation;
